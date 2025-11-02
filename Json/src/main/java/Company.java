05c8e57d9375ad333c391fb4762f6091bd917a09
kalthoff25 <kev.al@gmx.de>
2025-11-02 20:08:45 +0100

    Company.java

---
+++ Json/src/main/java/Company.java
@@ -0,0 +1,23 @@
+
+
+public class Company {
+    private String name;
+    private String headquarter_city;
+    private boolean isFromKarlsruhe;
+
+    public Company() {}
+
+    public Company(String name, String headquarter_city, boolean isFromKarlsruhe) {
+        this.name = name;
+        this.headquarter_city = headquarter_city;
+        this.isFromKarlsruhe = isFromKarlsruhe;
+    }
+
+    public String getName() { return name; }
+    public String getHeadquarter_city() { return headquarter_city; }
+    public boolean isFromKarlsruhe() { return isFromKarlsruhe; }
+
+    public void setName(String name) { this.name = name; }
+    public void setHeadquarter_city(String headquarter_city) { this.headquarter_city = headquarter_city; }
+    public void setFromKarlsruhe(boolean fromKarlsruhe) { isFromKarlsruhe = fromKarlsruhe; }
+}
